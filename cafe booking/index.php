2e38642d5add867179027246a5d42675d9ff8946
razzkyz <141397426+razzkyz@users.noreply.github.com>
2024-02-20 16:14:36 +0700

    Update index.php

--- cafe booking/index.php
+++ cafe booking/index.php
@@ -13,7 +13,7 @@ if (isset($_SESSION['username'])) {
  
 if (isset($_POST['submit'])) {
     $email = $_POST['email'];
-    $password = ($_POST['password']);
+    $password = md5($_POST['password']);
  
     $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
     $result = mysqli_query($conn, $sql);
